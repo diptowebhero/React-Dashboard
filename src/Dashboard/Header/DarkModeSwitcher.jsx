aaa7c0cf1133e8d934e3faeba64129e08a7c13d8
diptowebhero <diptodas1672@gmail.com>
2023-06-09 16:15:32 +0600

    theme & color modified

--- src/Dashboard/Header/DarkModeSwitcher.jsx
+++ src/Dashboard/Header/DarkModeSwitcher.jsx
@@ -1,26 +1,29 @@
+import { useEffect, useState } from "react";
+
 const DarkModeSwitcher = () => {
-  // const [theme, setTheme] = useState(null);
+  const [theme, setTheme] = useState(null);
 
-  // useEffect(() => {
-  //   if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-  //     setTheme('dark');
-  //   }
-  //   else {
-  //     setTheme('light');
-  //   }
-  // }, [])
+  useEffect(() => {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      setTheme('dark');
+    }
+    else {
+      setTheme('light');
+    }
+  }, [])
 
-  // useEffect(() => {
-  //   if (theme === "dark") {
-  //     document.documentElement.classList.add("dark");
-  //   } else {
-  //     document.documentElement.classList.remove("dark");
-  //   }
-  // }, [theme]);
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
 
-  // const handleThemeSwitch = () => {
-  //   setTheme(theme === "dark" ? "light" : "dark");
-  // };
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+  console.log(handleThemeSwitch);
   return <div></div>;
 };
 
